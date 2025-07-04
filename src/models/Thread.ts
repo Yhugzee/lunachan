805be300690291152c26235b3d5cc4ef3c7eca78
Hugo <rodriguez.h@pm.me>
2025-07-04 13:05:16 +0200

    add Thread model with nested Message schema for MongoDB

---
+++ src/models/Thread.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const MessageSchema = new Schema({
+  id: String,
+  content: String,
+  createdAt: String,
+  authorId: String,
+});
+
+const ThreadSchema = new Schema({
+  id: String,
+  title: String,
+  createdAt: String,
+  messages: [MessageSchema],
+});
+
+export const Thread = models.Thread || model("Thread", ThreadSchema);
