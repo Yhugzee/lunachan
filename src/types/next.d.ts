6bf2e0d0de3bfe04bb53cfa110c847433aebcee2
Hugo <rodriguez.h@pm.me>
2025-07-24 16:04:33 +0200

    refactor: update `NextApiRequest` admin property comment for clarity and consistency

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,8 +1,7 @@
 import type { IAdminUser } from "@/models/Admin";
-import type { NextApiRequest } from "next";
 
 declare module "next" {
   interface NextApiRequest {
-    admin?: IAdminUser; // l’admin vérifié par le middleware
+    admin?: IAdminUser; // admin attaché par withAdminAuth()
   }
 }
